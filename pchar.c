955de48c4920a35c8712513fb9d408b26b8f7752
teebee09 <tijaniteebee09@gmail.com>
2022-12-18 20:23:11 -0800

    LIFO FIFO

--- pchar.c
+++ pchar.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
  * f_pchar - prints the char at the top of the stack,
  * followed by a new line
@@ -7,13 +6,12 @@
  * @counter: line_number
  * Return: no return
  */
-
 void f_pchar(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 
 	h = *head;
-	if (h)
+	if (!h)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
 		fclose(bus.file);
