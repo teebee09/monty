779f07debfdedf91cb50cab6e3d6e23ba70e3162
Tijani Oluwatobiloba <tijaniteebee09@gmail.com>
2022-12-19 04:58:15 +0100

    Update 01.m

--- tests/01.m
+++ tests/01.m
@@ -2,9 +2,9 @@ push 0
 push 1
 push 2
       push 3
-      		  pall
+                  pall           
   push 4
-    push 5
-      push    6
-      	push 7
+    push 5   
+      push    6      
+push 7
 pall
